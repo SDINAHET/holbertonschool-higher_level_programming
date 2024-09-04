bd49bda9aace8456e3c7910bee5bbbf44e3c6a8d
SDINAHET <stephane.dinahet@gmail.com>
2024-09-05 00:57:37 +0200

    fix task9 pycodestyle comentarie too long

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    last_digit = abs(number) % 10  # Get the last digit using modulus and abs to handle negative numbers
+    last_digit = abs(number) % 10
+# Get the last digit using modulus and abs to handle negative numbers
     print("{}".format(last_digit), end="")
     return last_digit
