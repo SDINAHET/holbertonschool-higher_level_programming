6845360e515aed3f61fbb5b63f0fbc267f2d8dfe
SDINAHET <stephane.dinahet@gmail.com>
2024-09-09 22:59:57 +0200

    update 6-main.py

--- python-test_driven_development/6-main.py
+++ python-test_driven_development/6-main.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-max_integer = __import__('6-max_integer').max_integer
-
-print(max_integer([1, 2, 3, 4]))
-print(max_integer([1, 3, 4, 2]))
-guillaume@ubuntu:~/$
-guillaume@ubuntu:~/$ ./6-main.py
 4
 4
 guillaume@ubuntu:~/$
@@ -17,3 +11,5 @@ guillaume@ubuntu:~/$ head -7 tests/6-max_integer_test.py
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
