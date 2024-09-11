0527a8a70618c57160c61e1d68eb11afeccd04cb
SDINAHET <stephane.dinahet@gmail.com>
2024-09-11 04:41:56 +0200

    update task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -13,10 +13,14 @@ def roman_to_int(roman_string):
     length = len(roman_string)
 
     for i in range(length):
+        current_value = roman_dict[roman_string[i]]
+        next_value = roman_dict[roman_string[i + 1]] if i < length - 1 else 0
+
         # If the current numeral is smaller than the next one, subtract it
-        if i < length - 1 and roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
-            total -= roman_dict[roman_string[i]]
+        # if i < length - 1 and current_value < next_value:
+        if current_value < next_value:
+            total -= current_value
         else:
-            total += roman_dict[roman_string[i]]
+            total += current_value
 
     return total
