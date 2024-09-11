7ff2f26b9e431b7ae534d0e36fe779fe18c1cde4
SDINAHET <stephane.dinahet@gmail.com>
2024-09-11 09:57:47 +0200

    beginning project 7

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        # Attempt to print the value as an integer
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        # Handle cases where value is not an integer
+        return False
