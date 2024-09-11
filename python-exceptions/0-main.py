314b7940cdb63ba096f6f2dc3c42b1a554b80575
SDINAHET <stephane.dinahet@gmail.com>
2024-09-11 14:43:42 +0200

    add file project8 task 0 1 2

--- python-exceptions/0-main.py
+++ python-exceptions/0-main.py
@@ -3,9 +3,9 @@ safe_print_list = __import__('0-safe_print_list').safe_print_list
 
 my_list = [1, 2, 3, 4, 5]
 
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, 2) # output: 12
+print("nb_print: {:d}".format(nb_print)) # output: nb_print: 2
+nb_print = safe_print_list(my_list, len(my_list)) # output: 12345
+print("nb_print: {:d}".format(nb_print)) # output:nb_print: 5
+nb_print = safe_print_list(my_list, len(my_list) + 2) # output: 12345
+print("nb_print: {:d}".format(nb_print)) # output:nb_print: 5
