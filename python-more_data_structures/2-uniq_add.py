c30e5ad6e7635ebdd86a8c9627f8db30c6b29d7a
SDINAHET <stephane.dinahet@gmail.com>
2024-09-11 04:51:06 +0200

    update task 2 pycodestyle

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    #create a set to store unique values
+    # Create a set to store unique values
     unique_integers = set(my_list)
-    #Sum all the unique_integers)
+    # Sum all the unique_integers)
     return sum(unique_integers)
