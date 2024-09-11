24c90d56f5398a2996e2a88e7a34ed48be853b0a
SDINAHET <stephane.dinahet@gmail.com>
2024-09-11 20:15:43 +0200

    update 2-safe_print_list.... except (ValueError, TypeError):

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
-        except IndexError:
-            break
+        except (ValueError, TypeError):
+            # Ignorer silencieusement les types non entiers
+            continue
     print()
     return count
