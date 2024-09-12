94445fae540394512267f18c0dd9a1e34fd2f7ce
SDINAHET <stephane.dinahet@gmail.com>
2024-09-12 23:53:34 +0200

    update task1 module documented and pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module provides a function `matrix_divided` that divides all elements
+of a matrix by a given divisor, ensuring the matrix structure and handling
+exceptions like division by zero.
+"""
 
 def matrix_divided(matrix, div):
     """Divides all elements of a matrix by a given divisor.
@@ -9,17 +14,24 @@ def matrix_divided(matrix, div):
 
     Raises:
         TypeError: If `matrix` is not a list of lists of integers/floats,
-                   or if `div` is not a number, or if rows of `matrix` are not of the same size.
+                   or if `div` is not a number, or if rows of `matrix`
+                   are not of the same size.
         ZeroDivisionError: If `div` is zero.
 
     Returns:
-        list of lists of float: The new matrix with divided values rounded to 2 decimal places.
+        list of lists of float: The new matrix with divided values rounded
+        to 2 decimal places.
     """
     # Check if matrix is a list of lists of int/float
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not all(isinstance(elem, (int, float)) for row in matrix for elem in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list) or not all(
+            isinstance(row, list) for row in matrix):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+
+    if not all(isinstance(elem, (int, float)) for row in matrix
+               for elem in row):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
 
     # Check if all rows in the matrix are of the same size
     row_length = len(matrix[0])
