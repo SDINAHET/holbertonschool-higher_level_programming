ae82875316e5a6b836f4e7c14dbb8ad7c301e433
SDINAHET <stephane.dinahet@gmail.com>
2024-09-12 19:11:45 +0200

    add file project8

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def print_square(size):
+    """Print a square with the character '#'."""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+def text_indentation(text):
+    """Print text with two new lines after each '.', '?' and ':'."""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    result = ""
+    for char in text:
+        result += char
+        if char in ".?:":
+            result += "\n\n"
+
+    # Print the result, removing any leading or trailing spaces from each line
+    print("\n".join([line.strip() for line in result.split("\n")]))
