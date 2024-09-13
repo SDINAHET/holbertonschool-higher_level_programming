5a9c1f89b8eb1f4c7cf07d483d9fd9424c86e20d
SDINAHET <stephane.dinahet@gmail.com>
2024-09-13 15:58:13 +0200

    update task 5-...py et txt

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,16 +10,13 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    result = ""
-    skip_space = False
-    for char in text:
-        if char in ['.', '?', ':']:
-            result += char + "\n\n"
-            skip_space = True
-        elif char == " " and skip_space:
-            continue
-        else:
-            result += char
-            skip_space = False
+    characters = ['.', '?', ':']
+    result = ''
+    i = 0
+    while i < len(text):
+        result += text[i]
+        if text[i] in characters:
+            result += '\n\n'
+        i += 1
 
-    print(result.strip(), end="")
+    print(result.strip())
