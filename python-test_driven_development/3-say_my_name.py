f55e355767c60cf98173beedd33f0e864ddf247d
SDINAHET <stephane.dinahet@gmail.com>
2024-09-13 17:38:39 +0200

    update task 3-s ....py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -20,4 +20,8 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print("My name is {} {}".format(first_name, last_name))
+    # Print name with proper spacing
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
