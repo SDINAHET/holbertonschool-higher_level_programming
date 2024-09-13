26910d84e44472c7fa55ab7e8fd5ba579971cf6a
SDINAHET <stephane.dinahet@gmail.com>
2024-09-13 23:27:41 +0200

    update task 3.txt et .py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,7 +5,7 @@ containing the first and last name provided as input.
 """
 
 
-def say_my_name(first_name, last_name=""):
+def say_my_name(first_name="", last_name=""):
     """Function that prints 'My name is <first name> <last name>'
 
     Args:
@@ -15,13 +15,17 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: If either `first_name` or `last_name` is not a string.
     """
-    if not isinstance(first_name, str):
+    if first_name is None and last_name is None:
+        raise TypeError("say_my_name() missing 2 required positional arguments: 'first_name' and 'last_name'")
+
+    if not isinstance(first_name, (str, type(None))):
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if not isinstance(last_name, (str, type(None))):
         raise TypeError("last_name must be a string")
 
-    # Print name with proper spacing
-    if last_name:
+    if first_name and last_name:
         print(f"My name is {first_name} {last_name}")
+    elif first_name:
+        print(f"My name is {first_name} ")
     else:
-        print(f"My name is {first_name} ", end='\n')
+        print("No name provided")
