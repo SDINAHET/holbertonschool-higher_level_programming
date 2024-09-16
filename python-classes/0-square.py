e3c1a679f6f1f6799167dbb18554f9462fcfee5d
SDINAHET <stephane.dinahet@gmail.com>
2024-09-16 16:44:53 +0200

    update doc module 0-..py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square to represent a square.
+
+The Square class is currently empty but will later be expanded
+with attributes and methods to perform various operations related
+to squares.
+
+Example:
+    You can create a Square instance like this:
+
+    my_square = Square()
+
+Author: Your Name
+"""
+
+
 class Square:
     """An empty class that defines a square."""
     pass
