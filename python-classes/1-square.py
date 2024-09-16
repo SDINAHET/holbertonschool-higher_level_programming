99ffa8883996168d267c3ec7c80d8c862f606b6c
SDINAHET <stephane.dinahet@gmail.com>
2024-09-16 15:00:54 +0200

    add files  task 0 and 1 project 9

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+class Square:
+    """Defines a square by a private instance attribute `size`."""
+
+    def __init__(self, size):
+        """Initialize the square with a private instance attribute size."""
+        self.__size = size  # Private attribute
