174aee86ac14a6ea72864e850196d65a773bb9dd
SDINAHET <stephane.dinahet@gmail.com>
2024-09-23 11:56:23 +0200

    add content of file 1 to 11-main.py for project 11

--- python_inheritance/1-my_list.py
+++ python_inheritance/1-my_list.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+class MyList(list):
+    """
+    A subclass of the built-in list that includes a method to print the list sorted in ascending order.
+
+    Public Methods:
+        - print_sorted(self): Prints the list sorted in ascending order.
+
+    Example:
+        >>> my_list = MyList()
+        >>> my_list.append(3)
+        >>> my_list.append(1)
+        >>> my_list.append(2)
+        >>> print(my_list)
+        [3, 1, 2]
+        >>> my_list.print_sorted()
+        [1, 2, 3]
+        >>> print(my_list)
+        [3, 1, 2]
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list sorted in ascending order.
+
+        This method does not modify the original list.
+
+        Example:
+            >>> my_list = MyList([5, 2, 9, 1])
+            >>> my_list.print_sorted()
+            [1, 2, 5, 9]
+        """
+        print(sorted(self))
