8780b4666f321b4001ec815e879673263910fd25
SDINAHET <stephane.dinahet@gmail.com>
2024-09-26 11:04:32 +0200

    add files unlessdoc

---
+++ python-inheritance/unless_doc/10-unlessdoc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
