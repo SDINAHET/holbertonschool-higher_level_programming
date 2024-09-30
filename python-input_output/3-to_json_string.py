50fc7bf5c84c6f4e40060dbccb090466dbe42431
SDINAHET <stephane.dinahet@gmail.com>
2024-09-30 14:42:49 +0200

    update task3 and4

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+This module provides a function `to_json_string` that returns the JSON
+representation of an object (string).
+
+Functions:
+    to_json_string(my_obj): Returns the JSON representation of an object.
+"""
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: The object to convert to JSON.
+
+    Returns:
+        str: The JSON representation of the object.
+
+    Description:
+        - The function uses the `json` module to serialize the object.
+        - If the object cannot be serialized, a TypeError is raised.
+
+    Example:
+        >>> to_json_string([1, 2, 3])
+        '[1, 2, 3]'
+
+        >>> to_json_string({"id": 12, "name": "John"})
+        '{"id": 12, "name": "John"}'
+    """
+    import json
+    return json.dumps(my_obj)
--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""
+This module provides a function `from_json_string` that returns an object
+(Python data structure) represented by a JSON string.
+
+Functions:
+    from_json_string(my_str): Returns an object represented by a JSON string.
+"""
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to convert.
+
+    Returns:
+        object: The Python data structure represented by the JSON string.
+
+    Description:
+        - The function uses the `json` module to deserialize the JSON string.
+        - If the JSON string doesn't represent a valid object, a
+        JSONDecodeError
+          will be raised.
+
+    Example:
+        >>> from_json_string("[1, 2, 3]")
+        [1, 2, 3]
+
+        >>> from_json_string('{"id": 12, "name": "John"}')
+        {'id': 12, 'name': 'John'}
+    """
+    import json
+    return json.loads(my_str)
