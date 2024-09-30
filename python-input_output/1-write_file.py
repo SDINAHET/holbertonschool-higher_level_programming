ea335d21eb68bc979d61dca156b77fe3f94861a2
SDINAHET <stephane.dinahet@gmail.com>
2024-09-30 13:51:31 +0200

    update pycodestyle task 1 and 2

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,6 +8,7 @@ Functions:
     number of characters written.
 """
 
+
 def write_file(filename="", text=""):
     """Writes a string to a text file (UTF8) and returns the number of
     characters written.
--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -8,6 +8,7 @@ Functions:
     the number of characters added.
 """
 
+
 def append_write(filename="", text=""):
     """Appends a string at the end of a text file (UTF8) and returns the number
     of characters added.
