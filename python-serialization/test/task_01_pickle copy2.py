e0b305dacfc00a9ee60cd97e06a314f7a7f64c0e
SDINAHET <stephane.dinahet@gmail.com>
2024-10-01 21:00:55 +0200

    update task_01

--- python-serialization/test/task_01_pickle copy2.py
+++ python-serialization/test/task_01_pickle copy2.py
@@ -8,6 +8,7 @@ Class:
                   Provides methods to serialize and deserialize objects.
 """
 
+
 import pickle
 
 
@@ -58,8 +59,8 @@ class CustomObject:
         try:
             with open(filename, 'wb') as f:
                 pickle.dump(self, f)
-        except Exception:
-            pass  # Ignore any serialization errors
+        except Exception as e:
+            pass  # Use pass to avoid "Expected indented block" error
 
     @classmethod
     def deserialize(cls, filename):
@@ -76,6 +77,8 @@ class CustomObject:
         """
         try:
             with open(filename, 'rb') as f:
-                return pickle.load(f)
-        except (FileNotFoundError, pickle.UnpicklingError):
+                obj = pickle.load(f)
+            return obj
+        except (FileNotFoundError, pickle.UnpicklingError) as e:
+            pass  # Use pass to avoid "Expected indented block" error
             return None
