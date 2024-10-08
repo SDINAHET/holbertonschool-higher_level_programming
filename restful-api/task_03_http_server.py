d3399e1f36da6a88eb364fcc4e4607bc21302ae6
SDINAHET <stephane.dinahet@gmail.com>
2024-10-08 22:34:06 +0200

    update task3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -3,35 +3,26 @@
 Simple HTTP Server
 
 This script sets up a basic HTTP server using the http.server module.
-It handles different endpoints and serves
-text or JSON responses depending on the path requested by the client.
-
-Classes:
-    SimpleHTTPRequestHandler: Subclass of BaseHTTPRequestHandler that
-    implements GET request handling.
-
-Functions:
-    run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler,
-    port=8000):
-        Starts the HTTP server using the provided handler and server class on
-        the specified port.
+It handles different endpoints and serves text or JSON responses depending
+on the path requested by the client, as well as static files from the
+current directory.
 """
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import http.server
+import socketserver
 import json
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+class CustomHTTPRequestHandler(Handler):
     """
-    A simple HTTP request handler that handles GET requests for various
-    endpoints.
-
-    Methods:
-        do_GET(self):
-            Handles GET requests and routes them to specific endpoints.
+    A custom HTTP request handler that extends SimpleHTTPRequestHandler
+    to handle specific endpoints.
     """
 
     def set_headers(self, status_code, content_type):
@@ -41,35 +32,16 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        """
-        Handles GET requests and returns different responses based on the
-        requested path.
-
-        Supported paths:
-            - '/': Returns a plain text message "Hello, this is a simple API!".
-            - '/data': Returns a JSON object with sample data.
-            - '/status': Returns a JSON object indicating the API status as
-            "OK".
-            - '/info': Returns a JSON object with version information and a
-            description of the API.
-            - Any other path: Returns a 404 error with a JSON message
-            indicating that the endpoint is not found.
-        """
+        """Handles GET requests and returns different responses based on the
+        requested path."""
         logging.info(f"Request path: {self.path}")
 
         if self.path == '/':
-            # self.send_response(200)
-            # self.send_header('Content-type', 'text/plain')
-            # self.end_headers()
             self.set_headers(200, 'text/plain')
             self.wfile.write(b'Hello, this is a simple API!')
 
         elif self.path == '/data':
-            # self.send_response(200)
-            # self.send_header('Content-type', 'application/json')
-            # self.end_headers()
             self.set_headers(200, 'application/json')
-            # Sample JSON data
             data = {
                 "name": "John",
                 "age": 30,
@@ -78,9 +50,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(data).encode('utf-8'))
 
         elif self.path == '/status':
-            # self.send_response(200)
-            # self.send_header('Content-type', 'application/json')
-            # self.end_headers()
             self.set_headers(200, 'application/json')
             status_data = {
                 "status": "OK"
@@ -88,9 +57,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(status_data).encode('utf-8'))
 
         elif self.path == '/info':
-            # self.send_response(200)
-            # self.send_header('Content-type', 'application/json')
-            # self.end_headers()
             self.set_headers(200, 'application/json')
             info_data = {
                 "version": "1.0",
@@ -99,9 +65,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(info_data).encode('utf-8'))
 
         else:
-            # self.send_response(404)
-            # self.send_header('Content-type', 'application/json')
-            # self.end_headers()
+            # Gestion des erreurs pour les endpoints non définis
             self.set_headers(404, 'application/json')
             error_data = {
                 "error": "Endpoint not found"
@@ -110,26 +74,13 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
 
 def run(
-    server_class=HTTPServer,
-    handler_class=SimpleHTTPRequestHandler,
-    port=8000
-):
-    """
-    Starts the HTTP server.
-
-    Args:
-        server_class (type): The HTTP server class to use (default is
-        HTTPServer).
-        handler_class (type): The request handler class to use (default is
-        SimpleHTTPRequestHandler).
-        port (int): The port number to run the server on (default is 8000).
-
-    This function sets up the server and keeps it running to handle incoming
-    requests.
-    """
-    server_address = ('', port)
+        server_class=http.server.HTTPServer,
+        handler_class=CustomHTTPRequestHandler
+        ):
+    """Starts the HTTP server."""
+    server_address = ('', PORT)
     httpd = server_class(server_address, handler_class)
-    logging.info(f'Starting http server on port {port}...')
+    logging.info(f'Starting http server on port {PORT}...')
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
