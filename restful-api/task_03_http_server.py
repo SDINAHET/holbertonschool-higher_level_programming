d3988a9561fa91088f05a37770da8925b268e4dd
SDINAHET <stephane.dinahet@gmail.com>
2024-10-08 19:13:00 +0200

    update pycodestyle task3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,11 +1,52 @@
 #!/usr/bin/python3
+"""
+Simple HTTP Server
+
+This script sets up a basic HTTP server using the http.server module.
+It handles different endpoints and serves
+text or JSON responses depending on the path requested by the client.
+
+Classes:
+    SimpleHTTPRequestHandler: Subclass of BaseHTTPRequestHandler that
+    implements GET request handling.
+
+Functions:
+    run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler,
+    port=8000):
+        Starts the HTTP server using the provided handler and server class on
+        the specified port.
+"""
+
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
+
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    """
+    A simple HTTP request handler that handles GET requests for various
+    endpoints.
+
+    Methods:
+        do_GET(self):
+            Handles GET requests and routes them to specific endpoints.
+    """
 
     def do_GET(self):
+        """
+        Handles GET requests and returns different responses based on the
+        requested path.
+
+        Supported paths:
+            - '/': Returns a plain text message "Hello, this is a simple API!".
+            - '/data': Returns a JSON object with sample data.
+            - '/status': Returns a JSON object indicating the API status as
+            "OK".
+            - '/info': Returns a JSON object with version information and a
+            description of the API.
+            - Any other path: Returns a 404 error with a JSON message
+            indicating that the endpoint is not found.
+        """
         if self.path == '/':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
@@ -52,11 +93,30 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             }
             self.wfile.write(json.dumps(error_data).encode('utf-8'))
 
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+
+def run(
+    server_class=HTTPServer,
+    handler_class=SimpleHTTPRequestHandler,
+    port=8000
+):
+    """
+    Starts the HTTP server.
+
+    Args:
+        server_class (type): The HTTP server class to use (default is
+        HTTPServer).
+        handler_class (type): The request handler class to use (default is
+        SimpleHTTPRequestHandler).
+        port (int): The port number to run the server on (default is 8000).
+
+    This function sets up the server and keeps it running to handle incoming
+    requests.
+    """
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print(f'Starting http server on port {port}...')
     httpd.serve_forever()
+
 
 if __name__ == "__main__":
     run()
