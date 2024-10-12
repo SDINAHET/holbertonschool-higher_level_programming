569d36d794be4a2f2d1511391b4dc1be9a293ab6
SDINAHET <stephane.dinahet@gmail.com>
2024-10-12 22:08:56 +0200

    upddate t5

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-from flask import Flask, jsonify, request #
-from flask_httpauth import HTTPBasicAuth #
+from flask import Flask, jsonify, request
+from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 # 1b
 from werkzeug.security import generate_password_hash, check_password_hash # 2
