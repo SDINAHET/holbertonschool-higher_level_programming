c7fa07e69983ad5e2bde84fec4161d6e9b40c15d
SDINAHET <stephane.dinahet@gmail.com>
2024-10-12 21:59:57 +0200

    update t5

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -40,12 +40,12 @@ def basic_protected():
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    username = data.get("username")
-    password = data.get("password")
+    username = data.get('username')
+    password = data.get('password')
     user = users.get(username)
     if user and check_password_hash(user['password'], password):
         # Create a JWT token including the user role
-        access_token = create_access_token(identity={"username": user["username"], "role": user["role"]})
+        access_token = create_access_token(identity={'username': user['username'], "role": user['role']})
 #        return jsonify({"access_token": "<JWT_TOKEN>"})  # access_token
         return jsonify({access_token=access_token})  # access_token
     return jsonify({"error": "Invalid credentials"}), 401
