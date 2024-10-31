0974dbcd92a14852cdae96d4c957704e29d41f64
SDINAHET <stephane.dinahet@gmail.com>
2024-10-31 10:39:01 +0100

    update task0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -7,8 +7,8 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
 -- Grant privileges to each user if they don't have any (adjust privileges if necessary)
 -- GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
--- GRANT SELECT ON *.* TO 'user_0d_2'@'localhost'; -- Adjust as necessary
+-- GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost'; -- Adjust as necessary
 
 -- Apply the changes
 FLUSH PRIVILEGES;
