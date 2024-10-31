3be6763f0d245ddbe32b86725f44a18ab7c481df
SDINAHET <stephane.dinahet@gmail.com>
2024-10-31 10:52:32 +0100

    update task0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -10,9 +10,9 @@ GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
 -- GRANT SELECT ON *.* TO 'user_0d_2'@'localhost'; -- Adjust as necessary
 
 -- Apply the changes
--- FLUSH PRIVILEGES;
+FLUSH PRIVILEGES;
 
 -- Show privileges for each user
--- SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
